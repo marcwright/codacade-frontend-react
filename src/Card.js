206f3fdedfa36b86fa8334e205e7fcae7cce10fe
Marc Wright <marc.wright@capitalone.com>
2018-09-01 14:49:30 -0400

    added styling to nav

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ export class Card extends Component {
   render () {
     
     return (
-      <div className="col-xl-4 col-lg-4 animated fadeIn card-hover" key={this.props.id}>
+      <div className="col-xl-4 col-lg-4 animated card-max-height fadeIn card-hover" key={this.props.id}>
         <div className="card box-shadow">
           <a href={this.props.githubLink} target="_blank" role="button">
             <img className="card-img-top" data-src={this.props.img} alt="Thumbnail [100%x225]"
@@ -14,12 +14,10 @@ export class Card extends Component {
           <div className="card-body">
             <p className="card-text text-center">{this.props.name.toUpperCase()} <span className="black">/ {this.props.appName.toUpperCase()}</span></p>
             <div className="container">
-              <div className="row">
-                <div className="btn-group btn-group-sm col-lg-12" >
-                  <a href={this.props.githubRepo} className="btn btn-dark text-center col-lg-6" target="_blank" role="button">Repo</a>
-                  <a href={this.props.publicURL} className="btn btn-success text-center col-lg-6" role="button" target="_blank" aria-pressed="true">Play</a>
-                </div>
-              </div>
+
+                  <a href={this.props.githubRepo} className="btn btn-light btn-outline-dark text-center col-lg-12" target="_blank" role="button">GitHub Repo</a>
+                  
+
             </div>
           </div>
         </div>
