43b2ab85005362e6358cfe2c9880173154b2cdde
Maren Woodruff <marenwoodruff@Marens-MBP.fios-router.home>
2018-09-03 08:11:19 -0400

    re-sized some images

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import classNames from 'classnames';
 
 export class Card extends Component {
-  
+
   render () {
 
     // var pcss = this.props.publicURL
@@ -13,17 +13,17 @@ export class Card extends Component {
     //   'publicCss': pcss
     // });
     var insertPublicLetter = (this.props.publicURL) ? <span class='publicLetter'>public</span> : ""
-    
-    
+
+
     return (
-      <div className="col-xl-4 col-lg-4 col-md-6 animated fadeIn" key={this.props.id}>
+      <div className="col-xl-4 col-lg-4 col-md-6 col-12 animated fadeIn" key={this.props.id}>
         <div className="card box-shadow">
           <a href={this.props.githubLink} target="_blank" role="button">
             <img className="card-img-top" data-src={this.props.img} alt="Thumbnail [100%x225]"
                 src={this.props.img} />
           </a>
           <div className="card-body">
-              <p className="card-text text-center">{this.props.name.toUpperCase()} 
+              <p className="card-text text-center">{this.props.name.toUpperCase()}
                 <span className="black">
                   <a href={this.props.githubLink} target="_blank" role="button">/{this.props.appName.toUpperCase()}{insertPublicLetter}</a>
                 </span>
